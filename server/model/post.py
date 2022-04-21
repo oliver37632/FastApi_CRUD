6834c58e40a90e03cf4734cb5f52b5a70f218937
KimSeungJin <klli1naver@gmail.com>
2022-04-21 09:54:12 +0900

    ➕ :: model.post

---
+++ server/model/post.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, VARCHAR, Integer, ForeignKey, DATETIME, text
+from sqlalchemy.orm import relationship
+
+from server.model.comment import Comment
+
+from server.model import Base
+
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(VARCHAR(20), nullable=False)
+    content = Column(VARCHAR(255), nullable=False)
+    create_at = Column(DATETIME, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    user_id = Column(VARCHAR(10), ForeignKey('user.id'))
+
+    comment = relationship(Comment, cascade="all,delete", backref="post")
