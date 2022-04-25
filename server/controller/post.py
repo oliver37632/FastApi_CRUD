08426c43bf1de9be86f84def1295faf248e29da7
KimSeungJin <klli1naver@gmail.com>
2022-04-25 13:27:33 +0900

    🐜 ::  controller.post

--- server/controller/post.py
+++ server/controller/post.py
@@ -5,7 +5,7 @@ from server.model.schemas.post import Posts
 from server.utils.auth import token_check
 
 
-from fastapi import APIRouter, status, Depends, Response
+from fastapi import APIRouter, status, Depends, HTTPException
 
 from fastapi_jwt_auth import AuthJWT
 
@@ -17,7 +17,7 @@ app = APIRouter(
 
 
 @app.post("", status_code=status.HTTP_201_CREATED)
-async def post(body: Posts, authorize: AuthJWT = Depends()):
+async def write_post(body: Posts, authorize: AuthJWT = Depends()):
     with session_scope() as session:
         token_check(authorize=authorize, type="access")
         user_id = authorize.get_jwt_subject()
@@ -36,7 +36,7 @@ async def post(body: Posts, authorize: AuthJWT = Depends()):
 
 
 @app.get("", status_code=status.HTTP_200_OK)
-async def get_post(authorize: AuthJWT=Depends()):
+async def get_list_post(authorize: AuthJWT=Depends()):
     with session_scope() as session:
         token_check(authorize=authorize, type="access")
 
@@ -56,19 +56,36 @@ async def get_post(authorize: AuthJWT=Depends()):
         }, 404
 
 
+@app.get("/list", status_code=status.HTTP_200_OK)
+def view_moer_post(postId: int, authorize: AuthJWT=Depends()):
+    with session_scope() as session:
+        token_check(authorize=authorize, type="access")
+        try:
+            get_post = session.query(Post).filter(Post.id == postId).first()
+
+            return {
+                "id": get_post.id,
+                "title": get_post.title,
+                "content": get_post.content,
+                "user_id": get_post.userid
+            }
+        except :
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NotFound")
+
+
 @app.delete("")
-async def post_delete(id: int, authorize: AuthJWT=Depends()):
+async def delete_post(postId: int, authorize: AuthJWT=Depends()):
     with session_scope() as session:
         token_check(authorize=authorize, type="access")
         user_id = authorize.get_jwt_subject()
 
-        post_del = session.query(Post).filter(Post.id == id, Post.user_id == user_id).first()
+        post_del = session.query(Post).filter(Post.id == postId, Post.user_id == user_id).first()
 
         if post_del:
             session.delete(post_del)
             session.commit()
 
-            return Response(status_code=status.HTTP_204_NO_CONTENT)
+            raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="success")
 
         else:
-            return Response(status_code=status.HTTP_404_NOT_FOUND)
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NotFound")
